571f6218e8163b0c2d6ec40f8cc69afd045b521a
lnalice <rara9915@gmail.com>
2024-09-26 14:32:55 +0900

    Add BasicContainer

---
+++ src/style/div.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const BasicContainer = styled.div`
+  display : block;
+  text-align : center;
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 20px;
+`;
+
+export default BasicContainer;
