b1bee73ff04437a1e65f18f3ed9fe35740da8af9
lnalice <rara9915@gmail.com>
2024-09-26 14:32:35 +0900

    Fix description of newCommander

--- src/helper/newCommander.ts
+++ src/helper/newCommander.ts
@@ -1,8 +1,8 @@
 /**
  * new command "ACTION SCENE_ID TB_IDs"
- * @param {string} action 명령을 보낼 문자열
- * @param {string} scene 명령을 보낼 문자열
- * @param {string[]} robotIDs 명령을 보낼 문자열
+ * @param {string} action 요청할 움직임(MOVE MODULE MIN HOME RESET SCENE)
+ * @param {string} scene 움직임을 위한 데이터를 쓸 scene 선택
+ * @param {string[]} robotIDs 움직임 요청할 로봇 리스트
  */
 function newCommander(action: string, scene: string, robotIDs: string[]): string
 {
@@ -14,7 +14,7 @@ function newCommander(action: string, scene: string, robotIDs: string[]): string
     
     let cmd: string = action.toUpperCase() + " "  + scene + " " + robotIDs_str;
 
-    console.log("[New Command]", cmd);
+    console.log("[New Command] ", cmd);
 
     return cmd;
 }
