4ee8f06fda287a30f1173e4960481761c7efe57d
lnalice <rara9915@gmail.com>
2024-09-26 15:39:27 +0900

    Add publish (scene buttons)

--- src/components/SceneButtons.js
+++ src/components/SceneButtons.js
@@ -1,20 +1,26 @@
 import BasicButton from "../style/button.js";
 import BasicContainer from "../style/div.js";
+import newCommander from "../helper/newCommander.ts";
+import reactCommander from "../publishers/reactCommander.ts";
 
 function SceneButtons () {
+	const sceneList= process.env.REACT_APP_SCENE_LIST.split(",")
+	const robotList = process.env.REACT_APP_ROBOT_LIST.split(",")
+
+	const pubScene = (sceneID) => {
+		reactCommander(newCommander("SCENE", sceneID, robotList))
+	}
 
     return(
         <>
             <h1>scene test buttons</h1>
 			<BasicContainer>
-					<BasicButton id="scene_1">scene 1</BasicButton>
-					<BasicButton id="scene_2-1">scene 2-1</BasicButton>
-					<BasicButton id="scene_2-2">scene 2-2</BasicButton>
-					<BasicButton id="scene_3-1">scene 3-1</BasicButton>
-					<BasicButton id="scene_3-2">scene 3-2</BasicButton>
-					<BasicButton id="scene_4-1">scene 4-1</BasicButton>
-					<BasicButton id="scene_4-2">scene 4-2</BasicButton>
-					<BasicButton id="scene_4-3">scene 4-3</BasicButton>
+				{sceneList.map((item) => (
+					<BasicButton key={item} name ={item} 
+						onClick={(e) => {pubScene(e.target.name)}}>
+						{item}
+					</BasicButton>
+				))}
 			</BasicContainer>
         </>
     )
